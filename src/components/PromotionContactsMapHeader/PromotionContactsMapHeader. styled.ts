ee677fe7e0f691fcff1562bf2ec541d59882abe8
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-10-06 08:16:11 +0300

    add shops list styles

--- src/components/PromotionContactsMapHeader/PromotionContactsMapHeader. styled.ts
+++ src/components/PromotionContactsMapHeader/PromotionContactsMapHeader. styled.ts
@@ -1,12 +1,56 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
 
-export const ShopsListBtn = styled.button``;
+export const ShopsListBtn = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: ${({ theme }) => theme.spacing(6)};
+  border: none;
+  background-color: #f4f6f9;
 
-export const Title = styled.span``;
+  & > svg {
+    color: #2e305b;
+  }
+`;
 
-export const ListWrap = styled.div``;
+export const Title = styled.span`
+  color: #383e45;
+  font-family: ${({ theme }) => theme.fontFamily.geologica};
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.4;
+`;
+
+export const ListWrap = styled.div`
+  position: absolute;
+  z-index: 1000;
+  top: 100%;
+  left: 0px;
+  width: 100%;
+  max-height: 320px;
+  background-color: #f4f6f9;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 3px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #f6f6f9;
+    border-radius: 100px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #a282f7;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+`;
 
 export const List = styled.ul``;
 
--- src/components/PromotionContactsMapShop/PromotionContactsMapShop.styled.ts
+++ src/components/PromotionContactsMapShop/PromotionContactsMapShop.styled.ts
@@ -1,13 +1,46 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.label``;
+export const Container = styled.label`
+  display: block;
+  padding: ${({ theme }) => theme.spacing(6)};
+  cursor: pointer;
 
-export const TitleWrap = styled.span``;
+  &:not(:last-of-type) {
+    border-bottom: 1px solid #e3e7ed;
+  }
+`;
 
-export const Name = styled.span``;
+export const TitleWrap = styled.span`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing()};
+`;
 
-export const Address = styled.span``;
+export const Name = styled.span`
+  color: #383e45;
+  font-family: ${({ theme }) => theme.fontFamily.geologica};
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.4;
+`;
 
-export const WorkSchedule = styled.span``;
+export const Address = styled.span`
+  color: #383e45;
+  font-family: ${({ theme }) => theme.fontFamily.geologica};
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.4;
+`;
 
-export const RadioBtn = styled.input``;
+export const WorkSchedule = styled.span`
+  color: #383e45;
+  font-family: ${({ theme }) => theme.fontFamily.geologica};
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.4;
+`;
+
+export const RadioBtn = styled.input`
+  position: absolute;
+  transform: scale(0);
+`;
