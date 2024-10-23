3fdf6189cd863f88f949e9ecb3a5ccb66b97111c
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-10-23 08:11:26 +0300

    add redirectTo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from '@GeneralComponents/SharedLayout';
 import { usePromotions } from '@/hooks';
 import { useAuthStore } from '@/store/store';
-import { selectRefresh } from '@/store/auth/selectors';
+import { selectIsRefreshing, selectRefresh } from '@/store/auth/selectors';
 
 const NotFoundPage = lazy(() => import('@/pages/NotFoundPage'));
 const ServiceUnavailablePage = lazy(
@@ -15,37 +15,48 @@ const CabinetPage = lazy(() => import('@/pages/CabinetPage'));
 const PromotionsPage = lazy(() => import('@/pages/PromotionsPage'));
 const PromotionDetailsPage = lazy(() => import('@/pages/PromotionDetailsPage'));
 const RegisterCodePage = lazy(() => import('@/pages/RegisterCodePage'));
+const PrivateRoute = lazy(() => import('@GeneralComponents/PrivateRoute'));
 
 const App: FC = () => {
   usePromotions();
   const refresh = useAuthStore(selectRefresh);
+  const isRefreshing = useAuthStore(selectIsRefreshing);
 
   useEffect(() => {
-    refresh();
+    const refreshProfile = async () => {
+      await refresh();
+    };
+
+    refreshProfile();
   }, [refresh]);
 
   return (
-    <Routes>
-      <Route path={PagePaths.root} element={<SharedLayout />}>
-        <Route index element={<MainPage />} />
-        <Route path={PagePaths.root} element={<MainPage />} />
-        <Route path={PagePaths.cabinet} element={<CabinetPage />} />
-        <Route path={PagePaths.promotions} element={<PromotionsPage />} />
-        <Route
-          path={`${PagePaths.promotions}/:${PagePaths.dynamicParam}`}
-          element={<PromotionDetailsPage />}
-        />
-        <Route
-          path={`${PagePaths.code}/:${PagePaths.dynamicParam}`}
-          element={<RegisterCodePage />}
-        />
-        <Route
-          path={PagePaths.serviceUnavailable}
-          element={<ServiceUnavailablePage />}
-        />
-        <Route path='*' element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    !isRefreshing && (
+      <Routes>
+        <Route path={PagePaths.root} element={<SharedLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path={PagePaths.root} element={<MainPage />} />
+          <Route
+            path={PagePaths.cabinet}
+            element={<PrivateRoute element={<CabinetPage />} />}
+          />
+          <Route path={PagePaths.promotions} element={<PromotionsPage />} />
+          <Route
+            path={`${PagePaths.promotions}/:${PagePaths.dynamicParam}`}
+            element={<PromotionDetailsPage />}
+          />
+          <Route
+            path={`${PagePaths.code}/:${PagePaths.dynamicParam}`}
+            element={<RegisterCodePage />}
+          />
+          <Route
+            path={PagePaths.serviceUnavailable}
+            element={<ServiceUnavailablePage />}
+          />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    )
   );
 };
 
